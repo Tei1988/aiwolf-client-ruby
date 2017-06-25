409d7aafd302d63786e6b853c4d61dcba1c0f875
Tei1988 <github@grifola.xyz>
2017-06-25 23:28:18 +0900

    :bug:append Aiwolf::Base::Logger to Aiwolf::Base::CommandParser

--- lib/aiwolf/base/command_parser.rb
+++ lib/aiwolf/base/command_parser.rb
@@ -1,6 +1,10 @@
+require 'aiwolf/base/logger'
+
 module Aiwolf
   module Base
     module CommandParser
+      include Aiwolf::Base::Logger
+
       def parse_command(packet)
         logger.debug(packet[:request].downcase)
         send("inner_command_#{packet[:request].downcase}".to_sym, packet)
