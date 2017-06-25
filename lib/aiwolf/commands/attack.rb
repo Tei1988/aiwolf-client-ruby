ad1a0cb8f58cc7cf4f90b9d644fcca264cee67b8
Tei1988 <github@grifola.xyz>
2017-06-25 23:55:14 +0900

    :bug:remove not condition

--- lib/aiwolf/commands/attack.rb
+++ lib/aiwolf/commands/attack.rb
@@ -3,7 +3,7 @@ module Aiwolf
     module Attack
       def inner_command_attack(packet)
         hash = send(:command_attack, packet[:gameInfo])
-        return '' if hash.nil? || !hash.empty?
+        return '' if hash.nil? || hash.empty?
         JSON.generate(hash)
       end
     end
